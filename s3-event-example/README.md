7962b5faf51b6f05bdc8a659c0eb30ffc36c48ce
Kevin Cearns <kcearns@gmail.com>
2021-11-26 13:36:06 -0500

    Add S3 event example

---
+++ s3-event-example/README.md
@@ -0,0 +1,3 @@
+Template creates a DynamoDB table, S3 Bucket and a Lambda Function
+
+S3 -> Lambda -> DynamoDB
---
+++ s3-event-example/src/index.js
@@ -0,0 +1,24 @@
+const aws = require('aws-sdk')
+const uuid = require('uuid')
+
+const DynamoDB = new aws.DynamoDB.DocumentClient({ region: "us-east-1" })
+
+exports.handler = async (event) => {
+    const key = event.Records[0].s3.object.key;
+
+    let params = {
+        TableName: process.env.DB_NAME,
+        Item: {
+            id: uuid.v4(),
+            image: key,
+        }
+    }    
+
+    try {
+        await DynamoDB.put(params).promise()
+        console.log({"success": true, "msg": params})
+    } catch (err) {
+        console.log({"success": false, "msg": err.message})
+    }
+};
+              
